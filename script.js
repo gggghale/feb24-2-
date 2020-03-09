86052a9751df604db466431492a88e4f0aa78830
gggghale <falquezamiguel@gmail.com>
2020-03-09 13:18:50 +0800

    third commit

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function createPerson(name, gender, age){
 function Person (name,gender,age){
 
-    let p = {};
-    p.name = name;
-    p.gender = gender;
-    p.age = age;
-    return p;
     this.name = name;
     this.gender= gender;
     this.age = age;
+class Person{
+    constructor(name, gender, age){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+    walk(distance){
+        console.log(`${this.name} can walk up to ${distance} miles`)
+    }
 }
-let juan = createPerson("Juan Dela Cruz","male",25);
 
-console.log(`${juan.name} is ${juan.age} years old`) 
 let juan = new Person("Juan Dela Cruz", "Male", 35);
 document.write(`<h2>${juan.name} is a ${juan.gender} person </h2>`) 
+let juan = new Person ("Juan Dela Cruz", "male", 34);
+juan.walk(5); 
